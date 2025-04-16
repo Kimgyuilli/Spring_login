8406ea8dddd9118125d1d6e387e1b805cc3c0e6c
Gyuill Kim <rlarbdlf222@gmail.com>
2025-04-16 17:41:44 +0900

    [feat] 유효성 검사 추가

--- src/main/java/com/example/login/User/dto/request/MemberUpdateReq.java
+++ src/main/java/com/example/login/User/dto/request/MemberUpdateReq.java
@@ -1,13 +1,24 @@
 package com.example.login.User.dto.request;
 
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
 
 @Getter
 @Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class MemberUpdateReq {
     private Long id;
+
+    @NotBlank(message = "이메일은 필수입니다.")
+    @Email(message = "이메일 형식이 아닙니다.")
     private String memberEmail;
+
+    @NotBlank(message = "이름은 필수입니다.")
     private String memberName;
+
+    @NotBlank(message = "비밀번호는 필수입니다.")
     private String memberPassword;
 }
