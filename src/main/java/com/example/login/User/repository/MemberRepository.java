f0750f1baf78fd5c535c8c7e33badbbb22b809fb
Gyuill Kim <rlarbdlf222@gmail.com>
2025-04-18 19:38:11 +0900

    feat: CustomUserDetails, CustomUserDetailsServervice 구현

--- src/main/java/com/example/login/User/repository/MemberRepository.java
+++ src/main/java/com/example/login/User/repository/MemberRepository.java
@@ -11,4 +11,8 @@ public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
     //    이메일로 회원 정보 조회(select * from member_table)
     Optional<MemberEntity> findByMemberEmail(String memberEmail);
 
+
+    Boolean existsByMemberEmail(String memberEmail);
+
+    MemberEntity findByMemberEmailIgnoreCase(String memberEmail);
 }
