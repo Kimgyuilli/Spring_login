6519592d698e7d4825b67202ecbcfd82f8b3f451
Gyuill Kim <rlarbdlf222@gmail.com>
2025-04-23 23:03:11 +0900

    feat: swagger 초기 설정

---
+++ src/main/java/com/example/login/Common/config/SwaggerConfig.java
@@ -0,0 +1,28 @@
+package com.example.login.Common.config;
+
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI apiInfo() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Login API")
+                        .description("Spring Boot + JWT 로그인 API 명세서")
+                        .version("v1.0"));
+    }
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public-api")
+                .pathsToMatch("/api/**")
+                .build();
+    }
+}
